9590225c22c2cb00571101d1761c4df58919e3f1
Jason Axelson <jason.axelson@gmail.com>
2011-09-18 14:56:22 -1000

    cleanup

--- android/src/me/heartmycity/UseGpsActivity.java
+++ android/src/me/heartmycity/UseGpsActivity.java
@@ -15,7 +15,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
