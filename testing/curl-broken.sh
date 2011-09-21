dbe7e4571925d93bcfd3127ad4433d2562115881
Jason Axelson <jason.axelson@gmail.com>
2011-09-20 22:32:05 -1000

    Testing scripts for curl

---
+++ testing/curl-broken.sh
@@ -0,0 +1,2 @@
+curl -H "Content-Type: application/json" -X POST -d '{"description":"des is descrip"}'  \
+    http://ec2-107-20-189-184.compute-1.amazonaws.com/json/problems/
---
+++ testing/curl-test.sh
@@ -0,0 +1,2 @@
+curl -H "Content-Type: application/json" -X POST -d '{"description":"des is descrip"}'  \
+    http://posttestserver.com/post.php\?dump
